3cb2fa0ac9973bd62b7c3a298c1fc7b8b8f7dd43
Aonix7 <128262983+Aonix7@users.noreply.github.com>
2026-05-10 14:28:55 +0200

    add 24h price change indicator

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 async function getPrices() {
-    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd";
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd&include_24hr_change=true";
 
     const response = await fetch(url);
     const data = await response.json();
 
-    document.getElementById("btc").innerText = "$" + data.bitcoin.usd;
-    document.getElementById("eth").innerText = "$" + data.ethereum.usd;
+    const btcChange = data.bitcoin.usd_24h_change.toFixed(2);
+    const ethChange = data.ethereum.usd_24h_change.toFixed(2);
+
+    document.getElementById("btc").innerHTML =
+        "$" + data.bitcoin.usd + " (" + btcChange + "%)";
+
+    document.getElementById("eth").innerHTML =
+        "$" + data.ethereum.usd + " (" + ethChange + "%)";
 }
 
 // запускаем функцию сразу
