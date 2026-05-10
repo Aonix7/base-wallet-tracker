c75e83510eceef8112daee9d11f67f4eb1409772
Aonix7 <128262983+Aonix7@users.noreply.github.com>
2026-05-10 13:53:46 +0200

    add price api logic

---
+++ script.js
@@ -0,0 +1,12 @@
+async function getPrices() {
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    document.getElementById("btc").innerText = "$" + data.bitcoin.usd;
+    document.getElementById("eth").innerText = "$" + data.ethereum.usd;
+}
+
+// запускаем функцию сразу
+getPrices();
